cf3ec51afd395850d4bd2449d76bacf512afdd9e
emanuelefavero <emafaux89@gmail.com>
2022-09-17 18:25:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Roll Two Dice Game
+
+A vanilla JavaScript app that rolls two dices and displays the result.
+
+_I built this mini app after following Angela Yu course on Udemy back in 2019._
+
+## How to use
+
+    - you can simply open the index.html file in your browser :)
---
+++ script.js
@@ -0,0 +1,57 @@
+// define array fro img src
+const imgArray = [
+    'images/dice1.png',
+    'images/dice2.png',
+    'images/dice3.png',
+    'images/dice4.png',
+    'images/dice5.png',
+    'images/dice6.png',
+]
+
+// random array number
+const randomNum1 = Math.floor(Math.random() * imgArray.length)
+const randomNum2 = Math.floor(Math.random() * imgArray.length)
+
+// assign img src to img
+function changeImg() {
+    document.querySelectorAll('img')[0].src = imgArray[randomNum1]
+    document.querySelectorAll('img')[1].src = imgArray[randomNum2]
+}
+
+// change title depending on who wins
+function whoWins() {
+    if (randomNum1 > randomNum2) {
+        document.querySelector('h1').textContent = 'Player 1 Wins! 🎉'
+    } else if (randomNum1 < randomNum2) {
+        document.querySelector('h1').textContent = 'Player 2 Wins! 🎉'
+    } else {
+        document.querySelector('h1').textContent = 'Draw...'
+    }
+    document.querySelector('h1').classList.add('display-title')
+}
+// call functions
+changeImg()
+setTimeout(whoWins, 500)
+
+// hide and display footer animation
+setInterval(() => {
+    document.querySelector('footer').classList.toggle('display-footer')
+}, 600)
+
+// change title color animation
+setInterval(() => {
+    document.querySelector('h1').classList.toggle('change-color')
+}, 1000)
+
+// change title to refresh page after seconds
+const refreshPage = () => {
+    document.querySelector('h1').textContent = 'Click here to roll the dice!'
+    document.querySelector('footer').classList.toggle('display-footer-2')
+}
+
+setTimeout(refreshPage, 3000)
+
+document.querySelector('h1').addEventListener('click', () => {
+    location.reload()
+    // return false;
+})
