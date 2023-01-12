b24f4efaf944ccaa30bb5f3e9e58f8416ef6764f
emanuelefavero <emafaux89@gmail.com>
2023-01-12 05:12:14 +0100

    fix typos and indentation

--- README.md
+++ README.md
@@ -8,5 +8,4 @@ _I built this mini app after following Angela Yu course on Udemy back in 2019._
 
     - you can simply open the index.html file in your browser :)
 
-
 [Check the working website on GiHub Pages](https://emanuelefavero.github.io/roll-two-dice/)
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// define array fro img src
+// define array for img src
 const imgArray = [
-    'images/dice1.png',
-    'images/dice2.png',
-    'images/dice3.png',
-    'images/dice4.png',
-    'images/dice5.png',
-    'images/dice6.png',
+  'images/dice1.png',
+  'images/dice2.png',
+  'images/dice3.png',
+  'images/dice4.png',
+  'images/dice5.png',
+  'images/dice6.png',
 ]
 
 // random array number
@@ -14,20 +14,20 @@ const randomNum2 = Math.floor(Math.random() * imgArray.length)
 
 // assign img src to img
 function changeImg() {
-    document.querySelectorAll('img')[0].src = imgArray[randomNum1]
-    document.querySelectorAll('img')[1].src = imgArray[randomNum2]
+  document.querySelectorAll('img')[0].src = imgArray[randomNum1]
+  document.querySelectorAll('img')[1].src = imgArray[randomNum2]
 }
 
 // change title depending on who wins
 function whoWins() {
-    if (randomNum1 > randomNum2) {
-        document.querySelector('h1').textContent = 'Player 1 Wins! 🎉'
-    } else if (randomNum1 < randomNum2) {
-        document.querySelector('h1').textContent = 'Player 2 Wins! 🎉'
-    } else {
-        document.querySelector('h1').textContent = 'Draw...'
-    }
-    document.querySelector('h1').classList.add('display-title')
+  if (randomNum1 > randomNum2) {
+    document.querySelector('h1').textContent = 'Player 1 Wins! 🎉'
+  } else if (randomNum1 < randomNum2) {
+    document.querySelector('h1').textContent = 'Player 2 Wins! 🎉'
+  } else {
+    document.querySelector('h1').textContent = 'Draw...'
+  }
+  document.querySelector('h1').classList.add('display-title')
 }
 // call functions
 changeImg()
@@ -35,23 +35,24 @@ setTimeout(whoWins, 500)
 
 // hide and display footer animation
 setInterval(() => {
-    document.querySelector('footer').classList.toggle('display-footer')
+  document.querySelector('footer').classList.toggle('display-footer')
 }, 600)
 
 // change title color animation
 setInterval(() => {
-    document.querySelector('h1').classList.toggle('change-color')
+  document.querySelector('h1').classList.toggle('change-color')
 }, 1000)
 
 // change title to refresh page after seconds
 const refreshPage = () => {
-    document.querySelector('h1').textContent = 'Click here to roll the dice!'
-    document.querySelector('footer').classList.toggle('display-footer-2')
+  document.querySelector('h1').textContent = 'Click here to roll the dice!'
+  document.querySelector('footer').classList.toggle('display-footer-2')
 }
 
 setTimeout(refreshPage, 3000)
 
+// refresh page on h1 click
 document.querySelector('h1').addEventListener('click', () => {
-    location.reload()
-    // return false;
+  location.reload()
+  // return false;
 })
